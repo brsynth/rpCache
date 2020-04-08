e486ea3c24bf6d4dfaa2d70d6b463c7829c24f6f
joan <joan.herisson@univ-evry.fr>
2020-04-08 17:53:53 +0200

    add total_size

--- rpCache.py
+++ rpCache.py
@@ -17,6 +17,54 @@ import time
 ################### rpCache  ##########################
 #######################################################
 
+from sys import getsizeof, stderr
+from itertools import chain
+from collections import deque
+try:
+    from reprlib import repr
+except ImportError:
+    pass
+
+def total_size(o, handlers={}, verbose=False):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, deque, dict, set and frozenset.
+    To search other containers, add handlers to iterate over their contents:
+
+        handlers = {SomeContainerClass: iter,
+                    OtherContainerClass: OtherContainerClass.get_elements}
+
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                   }
+    all_handlers.update(handlers)     # user handlers take precedence
+    seen = set()                      # track which object id's have already been seen
+    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:       # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        if verbose:
+            print(s, type(o), repr(o), file=stderr)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
+
 
 ## Class to generate the cache
 #
@@ -164,7 +212,7 @@ class rpCache:
             start = time.time()
             if not getattr(self, attribute):
                 self._processAttribute(attribute, dirname, attributes[attribute])
-                print(" ("+str(sys_getsizeof(getattr(self,attribute)))+" bytes)", end = '', flush=True)
+                print(" ("+str(total_size(getattr(self,attribute)))+" bytes)", end = '', flush=True)
             end = time.time()
             print(" (%.2fs)" % (end - start))
 
@@ -175,7 +223,7 @@ class rpCache:
             start = time.time()
             if not getattr(self, attribute):
                 self._processAttribute2([attribute, 'name_'+attribute], dirname, attributes[attribute])
-                print(" ("+str(sys_getsizeof(getattr(self,attribute)))+" "+str(sys_getsizeof(getattr(self,'name_'+attribute)))+" bytes)", end = '', flush=True)
+                print(" ("+str(total_size(getattr(self,attribute)))+" "+str(total_size(getattr(self,'name_'+attribute)))+" bytes)", end = '', flush=True)
             end = time.time()
             print(" (%.2fs)" % (end - start))
 
